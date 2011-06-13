648bcb8f95e0a1610b1bd317101ef55ec7cbbf9c
Ps0ke <ps0ke.mail@gmail.com>
2011-06-14 00:49:48 +0200

    Had to move global include file to ./ due to recursive path fuckup. Started login.php

---
+++ action/login.php
@@ -0,0 +1,7 @@
+<?php
+// Global includes
+include('../include.php');
+include('../strings/en_US.php'); // English localization for now
+
+echo('hello login world')
+?>
--- include.php
+++ include.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // Starting a session
 include('strings/pw.php'); // Loading passwords and login information
-include('connect-to-sql.php'); // Establishing connection to sql database
+include('general/connect-to-sql.php'); // Establishing connection to sql database
 include('strings/info.php'); // Loading general strings
 ?>
