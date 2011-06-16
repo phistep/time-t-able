e1c9c410aaef35b5046369192c11e7fd751f4e76
Ps0ke <ps0ke.mail@gmail.com>
2011-06-16 16:10:13 +0200

    Fixed encoding errors in german localizaton.

--- strings/de_DE.php
+++ strings/de_DE.php
@@ -23,7 +23,11 @@ define('STR_REGISTER_EMAIL', 'E-Mail:');
 define('STR_REGISTER_CAPTCHA', 'Captcha:');
 define('STR_REGISTER_GO_BUTTON', 'Lass mich loslegen!');
 define('STR_REGISTER_DATA_LEGEND', 'Deine Daten:');
-define('STR_REGISTER_DATA_INFO', 'Ich nehme deine Privatsphäre sehr ernst. Ich werde deine Email nur für Support usw. benutzen und sie niemals an Dritte weitergeben. Ich hasse Spam auch. Dein Password wird niemals in Klartext gespeichert werden.');
+define('STR_REGISTER_DATA_INFO', 'Ich nehme deine Privatsph&auml;re sehr ernst. Ich werde deine Email nur f&uuml;r Support usw. benutzen und sie niemals an Dritte weitergeben. Ich hasse Spam auch. Dein Password wird niemals in Klartext gespeichert werden.');
+
+// PUBLIC
+define('STR_PUBLIC_SHAREING', ' zeigt seinen Stundenplan online. Wenn du das auch willst, kannst du dich <a href="'.MAIN_URL.'action/register.php">hier</a> registrieren!');
+define('STR_PUBLIC_DISCLAIMER', 'Wenn Sie Missbrauch feststellen oder ihre Privatsph&auml;re verletzt sehen, melden Sie das an <a href="mailto:'.ABUSE_EMAIL.'">'.ABUSE_EMAIL.'</a> und erw&auml;hnen Sie "'.LOGO_NAME.' - Abuse - <BENUTZERNAME>" im Betreff. Ich &uuml;bernehme keine Verantwortung f&uuml;r jedwegen Missbrauch dieses Dienstes. Dies hier sind benutzergenerierte Daten, f&uuml;r die ich icht verantwortlich bin.');
 
 // MAIN
 define('STR_SAVE_BUTTON', 'Speichern');
@@ -35,21 +39,21 @@ define('STR_DAY_FRI', 'Freitag');
 define('STR_GENERAL_PREFERENCES', 'Allgemeine Einstellungen');
 define('STR_GENERAL_PERIOD', 'Jahr:');
 define('STR_GENERAL_CLASS', 'Klasse/Stufe:');
-define('STR_GENERAL_PUBLIC', 'Öffenlticher Stundenplan:');
-define('STR_GENERAL_LINK', 'Dein öffentlicher Link:');
-define('STR_GENERAL_DEL_SHOW', 'Account löschen');
-define('STR_GENERAL_DEL_CONFIRM', 'Passwort bestätigen:');
-define('STR_GENERAL_DEL_NOUNDO', 'Dies kann nicht rückgängig gemacht werden!');
+define('STR_GENERAL_PUBLIC', '&Ouml;ffenlticher Stundenplan:');
+define('STR_GENERAL_LINK', 'Dein &ouml;ffentlicher Link:');
+define('STR_GENERAL_DEL_SHOW', 'Account l&ouml;schen');
+define('STR_GENERAL_DEL_CONFIRM', 'Passwort best&auml;tigen:');
+define('STR_GENERAL_DEL_NOUNDO', 'Dies kann nicht r&uuml;ckg&auml;ngig gemacht werden!');
 define('STR_GENERAL_DEL_GO', 'Auf Wiedersehen!');
 define('STR_TIMES', 'Zeiten');
-define('STR_SUBJECTS_SUBJECTS', 'Fächer');
+define('STR_SUBJECTS_SUBJECTS', 'F&auml;cher');
 define('STR_SUBJECTS_NAME', 'Name:');
 define('STR_SUBJECTS_TEACHER', 'Lehrer:');
 define('STR_SUBJECTS_ROOM', 'Standart Raum:');
 define('STR_SUBJECTS_COLOR', 'Farbe:');
 define('STR_TABLE_TIMETABLE', 'Stundenplan');
-define('STR_TABLE_HELP', 'Verändere hier deine genaue Stundenplananordnung. Wenn das Fach in seinem Standartraum statt findet, lass die Textbox leer und der Standartraum wird hinzugefügt. Ansonsten füge den gesonderten Raum für diese spezielle Stunde unten ein.');
+define('STR_TABLE_HELP', 'Ver&auml;ndere hier deine genaue Stundenplananordnung. Wenn das Fach in seinem Standartraum statt findet, lass die Textbox leer und der Standartraum wird hinzugef&uuml;gt. Ansonsten füge den gesonderten Raum für diese spezielle Stunde unten ein.');
 define('STR_TABLE_LESSON', 'Stunde');
-define('STR_TABLE_CHOOSE', 'Wählen...');
+define('STR_TABLE_CHOOSE', 'W&auml;hlen...');
 
 ?>
